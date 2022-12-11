d9c63f412f705a219160feeeaba09357dc85e6d4
Kostiantyn Lytvynov <lytvynov.kostiantyn@gmail.com>
2022-12-11 11:10:29 +0200

    start

---
+++ cards.py
@@ -0,0 +1,9 @@
+from itertools import product
+from random import shuffle
+
+SUITS = ['Heart', 'Diamond', 'Club', 'Spade']
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+
+class Card:
+    pass
---
+++ players.py
@@ -0,0 +1,10 @@
+class Absplayer:
+    pass
+
+
+class Player(Absplayer):
+    pass
+
+
+class Bot(Absplayer):
+    pass
